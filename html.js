e1b80a60eaa1fe469200588d3e9deb7a791ceeb3
pcontreras14 <56200978+pcontreras14@users.noreply.github.com>
2019-12-06 20:14:58 -0800

    Add files via upload

---
+++ html.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Stencil";
+ctx.fillText("C And G Engineeeers LLC", canvas.width/2, canvas.height/2);
+ctx.textAlign = "center";
+ctx.fillStyle = rgb(20,20,50);
